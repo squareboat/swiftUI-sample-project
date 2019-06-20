d562e800fa0ad21de77729c23f658908f3af83e7
Vaibhav <vaibhav.bhasin@squareboat.com>
2019-06-20 11:19:34 +0530

    ADD: Vstack and Image

--- SampleBetaApp/ContentView.swift
+++ SampleBetaApp/ContentView.swift
@@ -10,7 +10,29 @@ import SwiftUI
 
 struct ContentView : View {
     var body: some View {
-        Text("Hello world")
+        VStack{
+            Image("squareboat")
+                .padding(.all, 20)
+                .frame(width: 200.0, height: 200.0, alignment: .center)
+                .scaledToFill()
+                .clipShape(Circle())
+                .overlay(
+                Circle().stroke(Color.blue,lineWidth: 4))
+                .shadow(radius: 10)
+                .offset(y: -100)
+                .padding(.bottom, -110)
+            Text("Welcome to")
+                .italic()
+                .color(.blue)
+                .padding(.top,20)
+            Text("SwiftUI Sample App")
+                .bold()
+                .color(.blue)
+                .padding(.bottom, 20)
+            
+            }.frame(minWidth: 0, maxWidth: .infinity)
+            .background(Color.blue.opacity(0.1
+            ))
     }
 }
 
