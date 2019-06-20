8814cda74a754bf9c27becfa363a4159250ca741
Vaibhav <vaibhav.bhasin@squareboat.com>
2019-06-20 11:24:10 +0530

    ADD: navigation Veiew

--- SampleBetaApp/ContentView.swift
+++ SampleBetaApp/ContentView.swift
@@ -10,29 +10,47 @@ import SwiftUI
 
 struct ContentView : View {
     var body: some View {
-        VStack{
-            Image("squareboat")
-                .padding(.all, 20)
-                .frame(width: 200.0, height: 200.0, alignment: .center)
-                .scaledToFill()
-                .clipShape(Circle())
-                .overlay(
-                Circle().stroke(Color.blue,lineWidth: 4))
-                .shadow(radius: 10)
-                .offset(y: -100)
-                .padding(.bottom, -110)
-            Text("Welcome to")
-                .italic()
-                .color(.blue)
-                .padding(.top,20)
-            Text("SwiftUI Sample App")
-                .bold()
-                .color(.blue)
-                .padding(.bottom, 20)
-            
-            }.frame(minWidth: 0, maxWidth: .infinity)
-            .background(Color.blue.opacity(0.1
-            ))
+        NavigationView {
+            VStack {
+                
+                Spacer()
+                
+                VStack{
+                    Image("squareboat")
+                        .padding(.all, 20)
+                        .frame(width: 200.0, height: 200.0, alignment: .center)
+                        .scaledToFill()
+                        .clipShape(Circle())
+                        .overlay(
+                            Circle().stroke(Color.blue,lineWidth: 4))
+                        .shadow(radius: 10)
+                        .offset(y: -100)
+                        .padding(.bottom, -110)
+                    Text("Welcome to")
+                        .italic()
+                        .color(.blue)
+                        .padding(.top,20)
+                    Text("SwiftUI Sample App")
+                        .bold()
+                        .color(.blue)
+                        .padding(.bottom, 20)
+                    
+                    }.frame(width: 500)
+                    .background(Color.blue.opacity(0.1
+                    ))
+                
+                Spacer()
+                
+                NavigationButton(destination: ListContentView(), label: {
+                    Text("Continue")
+                        .bold()
+                        .color(.blue)
+                        .padding([.leading,.trailing],50)
+                        .padding([.top,.bottom], 10)
+                }).border(Color.blue, width : 2, cornerRadius: 20)
+                    .padding(20)
+            }
+        }
     }
 }
 
