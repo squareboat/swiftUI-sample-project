64f58bfaf492b6d89e081a00a94c4bb405c46347
Vaibhav <vaibhav.bhasin@squareboat.com>
2019-06-20 10:42:58 +0530

    INITIAL

---
+++ SampleBetaApp/ListContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ListContentView.swift
+//  SampleBetaApp
+//
+//  Created by Squareboat User on 19/06/19.
+//  Copyright © 2019 Squareboat. All rights reserved.
+//
+
+import SwiftUI
+
+struct ListContentView : View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#if DEBUG
+struct ListContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ListContentView()
+    }
+}
+#endif
