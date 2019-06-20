4f9c00432d2668e38e6edabb2a434f4d8ef542d8
Vaibhav <vaibhav.bhasin@squareboat.com>
2019-06-20 11:03:51 +0530

    INITIAL COMMIT

--- SampleBetaApp/ContentView.swift
+++ SampleBetaApp/ContentView.swift
@@ -10,41 +10,7 @@ import SwiftUI
 
 struct ContentView : View {
     var body: some View {
-        VStack {
-            Spacer()
-            VStack{
-                Image("squareboat")
-                    .padding(.all, 20)
-                    .frame(width: 200.0, height: 200.0, alignment: .center)
-                    .scaledToFill()
-                    .clipShape(Circle())
-                    .overlay(
-                        Circle().stroke(Color.white,lineWidth: 4))
-                            .shadow(radius: 10)
-                            .offset(y: -130)
-                            .padding(.bottom, -130)
-                            Text("Welcome to")
-                                .italic()
-                                .color(.blue)
-                                .padding(.top,20)
-                                Text("SwiftUI Sample App")
-                                    .bold()
-                                    .color(.blue)
-                                    .padding(.bottom, 20)
-                
-                                    }.frame(width: 500)
-                                        .background(Color.blue.opacity(0.1
-                                        ))
-            Spacer()
-            NavigationButton(destination: ListContentView(), label: {
-                Text("Continue")
-                    .bold()
-                    .color(.blue)
-                    .padding([.leading,.trailing],50)
-                    .padding([.top,.bottom], 10)
-            }).border(Color.blue, width : 2, cornerRadius: 20)
-                .padding(20)
-        }
+        Text("Hello world")
     }
 }
 
